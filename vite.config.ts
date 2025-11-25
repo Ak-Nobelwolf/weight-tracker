78df95b8c434ecb7a14d31e03d6a0705da8b0c41
Ak-Nobelwolf <akshaytadakod@gmail.com>
2025-11-25 15:59:14 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "production" ? "/weight-tracker/" : "/",
+  base: mode === "production" ? "/" : "/",
   build: {
     outDir: "dist",
     sourcemap: false,
